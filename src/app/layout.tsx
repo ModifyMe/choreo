f4a080c960de21176e0add266887bde87bf7fedf
Peter Piško <piskop@guardiaris.com>
2025-12-04 13:35:16 +0100

    feat: Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import { Providers } from "@/components/providers";
 
 const geistSans = Geist({
@@ -30,6 +32,7 @@ export default function RootLayout({
       >
         <Providers>
           {children}
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
